7d20b57f4e5eadd2aac7eddaa8d785cd7b567ae1
Azprialdi Caesar Aqiila <132909309+Azprialdi@users.noreply.github.com>
2023-05-09 15:12:40 +0800

    Update pilih-matakuliah.php

--- views/krs-mahasiswa/pilih-matakuliah.php
+++ views/krs-mahasiswa/pilih-matakuliah.php
@@ -1,7 +1,7 @@
 <?php
 /** @var yii\web\View $this */
 ?>
-<h1>krs-mahasiswa/pilih-matakuliah</h1>
+<h1>krs-mahasiswa/pilih-matakuliah1</h1>
 
 <p>
     You may change the content of this page by modifying
